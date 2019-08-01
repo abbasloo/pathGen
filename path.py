5e58ce22681a4eb34c99051732a976f416e54941
Amin Abbasloo <abbasloo@cs.uni-bonn.de>
2019-08-01 21:40:01 +0200

    Add files via upload

--- path.py
+++ path.py
@@ -25,7 +25,7 @@ def Sphere(location,R,H_min,H_max):
 	path = []
 	for i in range(d):
 		for j in range(d):
-			if R*math.cos(np.pi/d*i)>H_min:
+			if R*math.cos(np.pi/d*i)>=H_min:
 				if R*math.cos(np.pi/d*i)<H_max:
 					p = [R*math.sin(np.pi/d*i)*math.cos(2*np.pi/d*j)+location[0], 
 					     R*math.sin(np.pi/d*i)*math.sin(2*np.pi/d*j)+location[1],
@@ -44,7 +44,7 @@ def Plane(location,P1,P2,H_min,H_max):
 	for i in range(d):
 		D = [P2[0]+dir[0]/d*i, P2[1]+dir[1]/d*i]
 		for k in range(d):
-			p = [D[0]+location[0],D[1]+location[1],(H_max-H_min)/d*k+location[2]]
+			p = [D[0]+location[0],D[1]+location[1],(H_max-H_min)/d*k+H_min+location[2]]
 			path.append(p)
 	return np.asarray(path)
 
@@ -60,12 +60,12 @@ P2_xyz = lla2ecef(P2[0],P2[1],P2[2])
 #print (P2_xyz)
 
 ########################
-xyz = Plane(location_xyz,P1,P2,-2,+18)
+xyz = Cylinder(location_xyz,20.0,-2,+18)
 #print (xyz)
 
 lla = ecef2lla(xyz)
 
-f = open('path_Plane.csv','wt')
+f = open('path_Cylinder.csv','wt')
 writer = csv.writer(f)
 writer.writerow(('lat','lon','alt'))
 for i in range(len(lla[0])):
@@ -80,8 +80,6 @@ fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
 ax.scatter(xyz[:,0],xyz[:,1],xyz[:,2],s=10,marker='o',color='b')
 ax.scatter(location_xyz[0],location_xyz[1],location_xyz[2],s=50,marker='*',color='r')
-ax.scatter(P1_xyz[0],P1_xyz[1],P1_xyz[2],s=50,marker='+',color='g')
-ax.scatter(P2_xyz[0],P2_xyz[1],P2_xyz[2],s=50,marker='+',color='g')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
@@ -114,12 +112,12 @@ ax.set_zlabel('Z')
 plt.show()
 
 ########################
-xyz = Cylinder(location_xyz,20.0,-2,+18)
+xyz = Plane(location_xyz,P1,P2,-2,+18)
 #print (xyz)
 
 lla = ecef2lla(xyz)
 
-f = open('path_Cylinder.csv','wt')
+f = open('path_Plane.csv','wt')
 writer = csv.writer(f)
 writer.writerow(('lat','lon','alt'))
 for i in range(len(lla[0])):
@@ -134,6 +132,8 @@ fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
 ax.scatter(xyz[:,0],xyz[:,1],xyz[:,2],s=10,marker='o',color='b')
 ax.scatter(location_xyz[0],location_xyz[1],location_xyz[2],s=50,marker='*',color='r')
+ax.scatter(P1_xyz[0],P1_xyz[1],P1_xyz[2],s=50,marker='+',color='g')
+ax.scatter(P2_xyz[0],P2_xyz[1],P2_xyz[2],s=50,marker='+',color='g')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
